bf9229a146e276e46876c0749c5c34ef83784b9a
Chintan Kotadia <chintan.kotadia@knectar.com>
2021-02-28 14:20:19 +0530

    Changes in routes and refactored it

--- app/routes/admin/index.js
+++ app/routes/admin/index.js
@@ -3,47 +3,46 @@ var router = express.Router();
 
 const { check, validationResult } = require('express-validator');
 
-const {VerifyApiToken} = require('../../middleware');
-const {CheckPermission} = require('../../middleware');
+const { VerifyApiToken } = require('../../middleware');
+const { CheckPermission } = require('../../middleware');
 
-const auth = require('./auth');
+//Validators
 const UserValidation = require('../../validators/admin/UserValidation');
-
 const CommonValidation = require('../../validators/admin/CommonValidation');
 
+// Routes
+const auth = require('./auth');
 const race = require('./race');
 const gender = require('./gender');
-const sexual_orientation = require('./sexual_orientation');
+const sexualOrientation = require('./sexual_orientation');
 const workout = require('./workout');
-const health_category = require('./health_category');
-
+const healthCategory = require('./health_category');
 
 router.post('/auth/signin', UserValidation.SignIn, auth.SignIn);
 
-router.get('/race', [VerifyApiToken, CheckPermission], race.list);
+router.get('/race/list', [VerifyApiToken, CheckPermission], race.list);
 router.post('/race/store', [VerifyApiToken, CheckPermission, CommonValidation.Validation], race.store);
 router.post('/race/edit/:id', [VerifyApiToken, CheckPermission, CommonValidation.Validation], race.update);
 router.post('/race/delete/:id', [VerifyApiToken, CheckPermission], race.destroy);
 
-router.get('/gender', [VerifyApiToken], gender.list);
+router.get('/gender/list', [VerifyApiToken], gender.list);
 router.post('/gender/store', [VerifyApiToken, CommonValidation.Validation], gender.store);
 router.post('/gender/edit/:id', [VerifyApiToken, CommonValidation.Validation], gender.update);
 router.post('/gender/delete/:id', [VerifyApiToken], gender.destroy);
 
-router.get('/sexual_orientation', [VerifyApiToken], sexual_orientation.list);
-router.post('/sexual_orientation/store', [VerifyApiToken, CommonValidation.Validation], sexual_orientation.store);
-router.post('/sexual_orientation/edit/:id', [VerifyApiToken, CommonValidation.Validation], sexual_orientation.update);
-router.post('/sexual_orientation/delete/:id', [VerifyApiToken], sexual_orientation.destroy);
+router.get('/sexual_orientation/list', [VerifyApiToken], sexualOrientation.list);
+router.post('/sexual_orientation/store', [VerifyApiToken, CommonValidation.Validation], sexualOrientation.store);
+router.post('/sexual_orientation/edit/:id', [VerifyApiToken, CommonValidation.Validation], sexualOrientation.update);
+router.post('/sexual_orientation/delete/:id', [VerifyApiToken], sexualOrientation.destroy);
 
-router.get('/workout', [VerifyApiToken], workout.list);
+router.get('/workout/list', [VerifyApiToken], workout.list);
 router.post('/workout/store', [VerifyApiToken, CommonValidation.Validation], workout.store);
 router.post('/workout/edit/:id', [VerifyApiToken, CommonValidation.Validation], workout.update);
 router.post('/workout/delete/:id', [VerifyApiToken], workout.destroy);
 
-router.get('/health_category', [VerifyApiToken], health_category.list);
-router.post('/health_category/store', [VerifyApiToken, CommonValidation.Validation], health_category.store);
-router.post('/health_category/edit/:id', [VerifyApiToken, CommonValidation.Validation], health_category.update);
-router.post('/health_category/delete/:id', [VerifyApiToken], health_category.destroy);
-
+router.get('/health_category/list', [VerifyApiToken], healthCategory.list);
+router.post('/health_category/store', [VerifyApiToken, CommonValidation.Validation], healthCategory.store);
+router.post('/health_category/edit/:id', [VerifyApiToken, CommonValidation.Validation], healthCategory.update);
+router.post('/health_category/delete/:id', [VerifyApiToken], healthCategory.destroy);
 
 module.exports = router
