67de5e4bd15d75a5274ed9dd468b7ac60efd4e7e
Chintan Kotadia <chintan.kotadia@knectar.com>
2021-02-28 13:52:46 +0530

    Refactored APIResponse class for returning various response across the application

--- app/helpers/APIResponse.js
+++ app/helpers/APIResponse.js
@@ -1,32 +1,44 @@
+/**
+ * Manages APIResponse across the system
+ *
+ * @class APIResponse
+ * @package app
+ * @subpackage helpers
+ */
 class APIResponse {
 
-	/* Return response for success
-	 * success status
-	 * 200 - success
-	 */
-	success = (message, res, data = []) => {
-		return res
-			.status(200)
-			.send({
-				outputs: data,
-				success: message
-			})
-	}
+  /**
+   * Used for returning success response
+   *
+   * @param {String} message
+   * @param {Object} res
+   * @param {Array} data
+   */
+  success = (message, res, data = []) => {
+    return res
+      .status(200)
+      .send({
+        output: data,
+        message: message
+      })
+  }
 
-  /* Return response for error
-	 * error status
-	 * 404 - data not found
-	 * 401 - invalid data
-	 * 500 - internal server error
-	 */
-	error = (status, message, res, data = []) => {
-		return res
-			.status(status)
-			.send({
-				outputs: data,
-				error: message
-			})
-	}
+  /**
+   * Used for returning error response
+   *
+   * @param {Integer} status
+   * @param {String} message
+   * @param {Object} res
+   * @param {Array} data
+   */
+  error = (status, message, res, data = []) => {
+    return res
+      .status(status)
+      .send({
+        output: data,
+        message: message
+      })
+  }
 
 }
 
