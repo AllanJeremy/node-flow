653d4bd85a80753fe63053be740b14b1a46811fc
Chintan Kotadia <kotadiachintan@gmail.com>
2021-03-03 19:08:07 +0530

    Updated middlewares code for excluding signin route.

--- app/middleware/HasPermission.js
+++ app/middleware/HasPermission.js
@@ -20,34 +20,40 @@ const validationLanguage = language.en.admin.validation;
 /**
  * Route Config
  */
-let routeConfig = require('../routes/admin/config');
-routeConfig = routeConfig.apiRoute;
+var AllRoutesConfig = require('../routes/admin/config');
+var routeConfig = AllRoutesConfig.apiRoute;
+var authRoutesConfig = AllRoutesConfig.authRoute;
+
 
 exports.verify = (req, res, next) => {
-  AdminPermission.findOne({
-    where: {admin_user_id: req.id }
-  }).then(response => {
+  if (req.originalUrl.replace('/admin', '') === authRoutesConfig.AUTH_LOGIN) {
+    return next();
+  } else {
+    AdminPermission.findOne({
+      where: {admin_user_id: req.id }
+    }).then(response => {
 
-    let exists = false;
-    let permissions = response.permissions.replace('{', '').replace('}', '');
-    let currentRoute = req.originalUrl;
-    currentRoute = currentRoute.replace('/admin','');
+      let exists = false;
+      let permissions = response.permissions.replace('{', '').replace('}', '');
+      let currentRoute = req.originalUrl;
+      currentRoute = currentRoute.replace('/admin','');
 
-    permissions.split(',').map((routeName, index) => {
-      var routeMatchName = routeConfig[routeName].name;
-      var routeRegex = routeMatchName.split(":id").join("([\\d-]+)");
-      routeRegex = "^" + routeRegex + "$";
-      if (currentRoute.match(routeRegex) !== null) {
-        exists = true;
-        next();
+      permissions.split(',').map((routeName, index) => {
+        var routeMatchName = routeConfig[routeName].name;
+        var routeRegex = routeMatchName.split(':id').join('([\\d-]+)');
+        routeRegex = '^' + routeRegex + '$';
+        if (currentRoute.match(routeRegex) !== null) {
+          exists = true;
+          next();
+        }
+      });
+
+      if (!exists) {
+        return ResponseHandler.error(res, 400, responseLanguage.permission_denied);
       }
+    })
+    .catch(err => {
+      return ResponseHandler.error(res, 500, err.message);
     });
-
-    if (!exists) {
-      return ResponseHandler.error(res, 400, responseLanguage.permission_denied);
-    }
-  })
-  .catch(err => {
-    return ResponseHandler.error(res, 500, err.message);
-  });
+  }
 }
