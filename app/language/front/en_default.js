ab9f3bc0dc669b02551d83781b02c54751c336b8
Chintan Kotadia <kotadiachintan@gmail.com>
2021-04-15 17:09:49 +0530

    [API-41] Added language labels for validation, sign up and confirm email screens.

--- app/language/front/en_default.js
+++ app/language/front/en_default.js
@@ -1,22 +1,59 @@
-exports.en = {
-    'welcome': {
-        'title': 'Welcome to Joyn',
-        'description': 'A place to find daily joy in community. We are so happy to have you.',
-        'button': 'Get Started' 
+exports.labels = {
+  'welcome': {
+    'title': 'Welcome to Joyn',
+    'description': 'A place to find daily joy in community. We are so happy to have you.',
+    'button': 'Get Started'
+  },
+  'login': {
+      'title': 'Log In',
+      'form': {
+        'email_label': 'Email',
+        'email_placeholder': 'Email',
+        'password_label': 'Password',
+        'password_placeholder': 'Password',
+        'button': 'Log In'
+      },
+      'forgot_password_link': 'Forgot password?',
+      'sign_up_link': {
+        'text1': 'Don’t have an account?',
+        'text2': 'Sign Up'
+      }
+  },
+  'signup': {
+    'title': 'Sign Up',
+    'form': {
+      'email_label': 'Email',
+      'email_placeholder': 'Email',
+      'password_label': 'Password',
+      'password_placeholder': 'Password',
+      'repeat_password_label': 'Repeat password',
+      'repeat_password_placeholder': 'Repeat password',
+      'button': 'Sign Up'
     },
-    'login': {
-        'title': 'Log In',
-        'form': {
-            'email_label': 'Email',
-            'email_placeholder': 'Email',
-            'passowrd_label': 'Password',
-            'passowrd_placeholder': 'Password',
-            'button': 'Log In'
-        },
-        'forgot_password_link': 'Forgot password?',
-        'sign_up_link': {
-            'text1': 'Don’t have an account?',
-            'text2': 'Sign Up'
-        }
+    'terms_and_policy': {
+      'text1': 'Signing up you agree to our',
+      'text2': 'Terms of Service',
+      'text3': 'and',
+      'text4': 'Privacy Policy',
+    },
+    'login_in_link': {
+      'text1': 'Already have an account?',
+      'text2': 'Log In'
     }
+  },
+  'confirm_email': {
+    'title': 'Please confrim your email',
+    'sub_title': 'Enter the confirmation code we sent to your email:',
+    'code': 'Code',
+    'send_again_button': 'Send code again',
+    'button': 'Continue',
+    'resend_success': 'Code was re-sent to your email address'
+  },
+  'validation': {
+    'required_fields': 'All fields are required',
+    'email_invalid': 'Please enter valid email',
+    'password_length_invalid': 'Atleast 8 characters in length',
+    'password_invalid': 'Should contain: Lower case letters (a-z), Upper case letters (A-Z), Numbers (0-9)',
+    'repeat_password': 'Repeat password does not match Password'
+  }
 }
