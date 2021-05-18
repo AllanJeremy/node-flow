3f4ed0922589d598b32c24dc3f107e709e9fa16d
Chintan Kotadia <kotadiachintan@gmail.com>
2021-05-18 18:40:54 +0530

    Updated avatar path in front api response.

---
+++ app/transformers/front/AvatarTransformer.js
@@ -0,0 +1,17 @@
+var fractal = require('fractal-transformer')();
+require('dotenv').config();
+
+class CommonTransformer {
+
+  transform = (data) => fractal(data, {
+    'id': 'id',
+    'avatar': function (data) {
+      return process.env.API_IMAGE_URL + '/avatar/' + data.get('name');
+    },
+    'status': function (data) {
+      return data.get('status');
+    }
+  });
+}
+
+module.exports = CommonTransformer;
