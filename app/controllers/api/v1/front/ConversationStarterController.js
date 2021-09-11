1544439230b2426851bc054eb7d30178d37e8032
Chintan Kotadia <kotadiachintan@gmail.com>
2021-09-11 16:26:54 +0530

    Removed commented code.

--- app/controllers/api/v1/front/ConversationStarterController.js
+++ app/controllers/api/v1/front/ConversationStarterController.js
@@ -192,28 +192,6 @@ class ConversationStarterController {
         });
 
         await Chat.createChannel(botUser, user);
-
-        // const client = Chat.getInstance();
-
-        // const channel = client.channel('messaging', {
-        //   members: [botUser.unique_id, user.unique_id],
-        //   created_by_id: botUser.unique_id,
-        //   creater_id: botUser.unique_id,
-        //   is_accepted: true,
-        //   sender_match_feedback_completed: false,
-        //   receiver_match_feedback_completed: false,
-        //   is_deleted: false,
-        //   is_deleted_by: '',
-        //   sender_id: botUser.unique_id,
-        //   receiver_id: user.unique_id
-        // });
-
-        // await channel.create();
-
-        // const message = await channel.sendMessage({
-        //   user_id: botUser.unique_id,
-        //   text: 'Hi ' + user.first_name  + '! ' + chatLanguage.default_message
-        // });
       } catch(e) {
         console.log(e);
       }
