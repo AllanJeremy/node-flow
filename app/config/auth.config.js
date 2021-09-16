3a2957fdc0f391625c9d77909b86b58ae87994a4
Chintan Kotadia <kotadiachintan@gmail.com>
2021-09-16 17:35:25 +0530

    Change token expiration time to 1 year

--- app/config/auth.config.js
+++ app/config/auth.config.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 module.exports = {
   secret: process.env.APP_SECRET, // this key will be used in generating JWT token
-  tokenExpiryTime: 86400, // token expiry time in seconds
-  expiryTime: 24 // token expiry time in hours
+  tokenExpiryTime: 31536000, // token expiry time in seconds
+  expiryTime: 8760 // token expiry time in hours
 };
