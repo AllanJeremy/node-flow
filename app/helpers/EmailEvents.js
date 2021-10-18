5e8e74f932b6f4eb4d953fec843c2f235acd6012
Chintan Kotadia <kotadiachintan@gmail.com>
2021-10-18 14:32:27 +0530

    Updated env variable name in event helper file.

--- app/helpers/EmailEvents.js
+++ app/helpers/EmailEvents.js
@@ -19,7 +19,7 @@ const KEYS = {
 class EmailEvents {
 
   init(action, data) {
-    if(process.env == 'production') {
+    if (process.env.APP_ENV == 'production') {
       switch(action) {
         case 'signup':
          return this.signup(data);
