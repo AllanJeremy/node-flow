e42953dd28091190d7a91cf97650eb1187e63abe
Allan Jeremy <aj.dev254@gmail.com>
2021-10-22 17:03:15 +0300

    [Database] Added prompt related models

---
+++ app/models/PromptOptions.js
@@ -0,0 +1,31 @@
+"use strict";
+const { Model } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  class PromptOption extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+
+  PromptOption.init(
+    {
+      text: DataTypes.STRING,
+      prompt_id: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "PromptOption",
+      tableName: "prompt_options",
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    }
+  );
+
+  return PromptOption;
+};
