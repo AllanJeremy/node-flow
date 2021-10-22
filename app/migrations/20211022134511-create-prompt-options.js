15b5ff71c96c444a66f3a57a290754f4afaf7d5c
Allan Jeremy <aj.dev254@gmail.com>
2021-10-22 17:57:38 +0300

    [Database] Added prompt related migrations

---
+++ app/migrations/20211022134511-create-prompt-options.js
@@ -0,0 +1,41 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("prompt_options", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      text: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      prompt_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "prompts",
+          key: "id",
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        field: "created_at",
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP(6)"),
+      },
+      updatedAt: {
+        allowNull: false,
+        field: "updated_at",
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP(6)"),
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("prompt_options");
+  },
+};
---
+++ app/migrations/20211022134524-create-user-prompts.js
@@ -0,0 +1,54 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("user_prompts", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      prompt_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "prompts",
+          key: "id",
+        },
+      },
+      prompt_option_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "prompt_options",
+          key: "id",
+        },
+        defaultValue: null,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        onDelete: "CASCADE",
+        references: {
+          model: "users",
+          key: "id",
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        field: "created_at",
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP(6)"),
+      },
+      updatedAt: {
+        allowNull: false,
+        field: "updated_at",
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP(6)"),
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("user_prompts");
+  },
+};
