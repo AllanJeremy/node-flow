6e8f3c2ed538e3aca58acd07626444d4a4cb8963
Allan Jeremy <aj.dev254@gmail.com>
2021-10-26 16:04:48 +0300

    [Routes] Added prompt admin routes

---
+++ app/routes/admin/router/prompt.js
@@ -0,0 +1,39 @@
+var express = require("express");
+var router = express.Router();
+
+/**
+ * Route Configs
+ */
+const routeConfig = require("../config");
+const apiRoute = routeConfig.apiRoute;
+
+/**
+ * Validators
+ */
+const CommonValidation = require("../../../validators/admin/CommonValidation");
+
+//
+const PromptController = require("../../../controllers/api/v1/admin/PromptController");
+
+/**
+ * Gender Routes
+ */
+
+router.get(apiRoute.PROMPT.LIST, PromptController.list);
+
+router.post(apiRoute.PROMPT.STORE, PromptController.store);
+
+router.patch(apiRoute.PROMPT.UPDATE, PromptController.updatePrompt);
+
+router.patch(
+  apiRoute.PROMPT.UPDATE_OPTION,
+  PromptController.updatePromptOption
+);
+
+router.delete(apiRoute.PROMPT.DELETE, PromptController.destroyPrompt);
+router.delete(
+  apiRoute.PROMPT.DELETE_OPTION,
+  PromptController.destroyPromptOption
+);
+
+module.exports = router;
