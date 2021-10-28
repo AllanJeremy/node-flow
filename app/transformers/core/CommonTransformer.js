39691b928245850ee2faf5e1dd448bc4bf21f2e7
Allan Jeremy <aj.dev254@gmail.com>
2021-10-28 19:08:24 +0300

    [Refactoring] Changed all core transformer class methods to be static There is no need to create an instance for any of the helpers hence the switch, which also reduces the amount of code we need to import/instantiate when using said helpers

--- app/transformers/core/CommonTransformer.js
+++ app/transformers/core/CommonTransformer.js
@@ -1,7 +1,7 @@
 var fractal = require("fractal-transformer")();
 
 class CommonTransformer {
-  transform = (data) =>
+  static transform = (data) =>
     fractal(data, {
       id: "id",
       name: "name",
